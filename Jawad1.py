40b58e77b2845de710e598d3183e94d9564e62f3
Jawadkarim <jksarteezi@gmail.com>
2023-03-03 11:43:32 +0500

    jawad1

---
+++ Jawad1.py
@@ -0,0 +1,11 @@
+print("\t"" MY PROFILE")
+name="Jawad"
+last_name="karim"
+programe="BS_RS_GIS"
+age=20
+semester=4
+print("Name:", name)
+print("last_name:", last_name)
+print("programe:", programe)
+print("age:", age)
+print("semester:", semester)
